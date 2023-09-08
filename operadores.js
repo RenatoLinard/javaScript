6010dbceac3d95341cad0f7a09b56282d839b485
RenatoLinard <renatolinardjr@gmail.com>
2023-09-08 06:13:31 -0400

    novos exercicios

---
+++ operadores.js
@@ -0,0 +1,13 @@
+//operadores aritméticos
+var soma = 7 + 5;
+var sub = 7 - 5;
+var div = 10 / 2;
+var mult = 10 * 4;
+var mod = 3 % 2;
+
+console.log(soma, sub, div, mult, mod);
+
+//concatenação entre strings com operador de adição
+console.log('hello' + 'world');
+
+
