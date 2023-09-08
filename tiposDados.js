8b823c7075907169872997026c748fe6079674c4
RenatoLinard <renatolinardjr@gmail.com>
2023-09-08 05:55:06 -0400

    novos exercicios

--- tiposDados.js
+++ tiposDados.js
@@ -1,7 +1,26 @@
 //vai imprimir o tipo
-var valor = 4
+var valor = 4;
 console.log(typeof valor);
 
 //Tipo de dados Array
+
+var meses = ['janeiro', 'favereiro', 'março', 'abril', 'maio', 'junho','julho',
+'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+
+//acessando um valor da Array
+console.log(meses[0]);
+console.log(meses[5]);
+
+//alterando um valor na Array
+meses[0] = 'JANEIRO';
+console.log(meses[0]);
+
+//contando a quantidade de elementos
+console.log(meses.length);
+
+//declarando uma constante
+const nome = 'Renato de Souza Linard Junior';
+console.log(nome);
+// uma const é uma variavel imutavel
 
 
